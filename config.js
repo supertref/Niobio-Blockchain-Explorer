5629a29bb71fe40c22f40f3bef57cd3204f12dfd
Vinicius <vinyvicente@gmail.com>
2017-12-11 23:18:41 -0200

    Config block explorer

--- config.js
+++ config.js
@@ -6,22 +6,8 @@ var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
     "krb": [
-		["krb.mypool.name", "http://krb.mypool.name:32351"],
-		["democats.org/pool/?name=karbowanec", "http://pool2.democats.org:7673"],
-		["pool.karbowanec.com", "http://pool.karbowanec.com:8117"],
-		["pool2.karbowanec.com", "http://pool2.karbowanec.com:8117"],
-		["krb.cryptonotepool.com", "http://5.189.135.137:8618"],
-		["krb.tfoto.com.ua", "http://178.150.34.202:8117"],
-		["krb.crypto-coins.club", "http://krb.crypto-coins.club:8118"],
-		["krb.sberex.com", "http://krb.sberex.com:7006"],
-		["amazon.sberex.com", "http://amazon.sberex.com:7006"],
-		["xcrypto.org", "http://xcrypto.org:8117"],
-		["krb.cryptomine.pro", "http://krb.cryptomine.pro:8117"],
-		["bityce.ddns.net", "http://bityce.ddns.net:9884"],
-		["pool.karbowanec.com.ua", "http://zacent.com.ua:8117"],
-		["krb-pool.pp.ua", "http://krb-pool.pp.ua:25417"],
-		["54.223.133.248", "http://54.223.133.248:8117"],
-		["bitcache.cc", "http://bitcache.cc:8117"]
-		
+		["nbr.4miner.start", "http://asia-nbr.4miner.me:3334"],
+		["nbr.4miner.medium", "http://asia-nbr.4miner.me:5556"],
+		["nbr.4miner.high", "http://asia-nbr.4miner.me:7778"]
     ]
 };
